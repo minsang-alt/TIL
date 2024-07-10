a6b7ea3a2887683c1755fc210363fa861cf1f234
minsang-alt <mazinger97@naver.com>
2024-07-10 22:33:03 +0900

    remove readme and modify nickname

--- blog-config.js
+++ blog-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: "EVO.blog",
   description: "지식과 경험을 기록하고 공유하는 학습 저장소",
-  author: "MinSang",
-  siteUrl: "https://evo.blog",
+  author: "EVO",
+  siteUrl: "https://evoblog.life/",
   links: {
     github: "https://github.com/minsang-alt",
     linkedIn: "https://linkedin.com",
